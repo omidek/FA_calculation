5429c21d00d78b29a628714543781f8018daf787
Omid Ekrami <o.ekrami@gmail.com>
2020-05-10 14:46:26 +0200

    Update c_fa_example.m

--- c_fa_example.m
+++ c_fa_example.m
@@ -3,18 +3,18 @@ load('sym_KU.mat');
 load('sample_faces.mat') % a set of 10 sample faces, synthetically made from a weighted average of real faces.
 mask=sym;
 
-%Calculating the signed difference between a face and its mirror, corrected for average DA
+%Calculating the signed difference between a face and its mirror
 [~,~,signed_diff,~] = calculate_FA(sample_faces,mask,'face'); 
 
 % the DA vector, calculated as the average of the signed differences
 DA_vector = mean(signed_diff,1); 
 DA_direction  = DA_vector'/norm(DA_vector);
 
-% projection of the signed differences onto the DA direction, this produces a DA score for each face
-DA_scores = signed_diff*DA_direction;
+% projection of the signed differences onto the DA direction and corrected for average DA, this produces F-DA score for each face
+FDA_scores = signed_diff*DA_direction - norm(DA_vector);
 
 % calculating the vector of projections for each face
-DA_projections = repmat(DA_scores,1,size(signed_diff,2)).*repmat(DA_direction',size(signed_diff,1),1);
+DA_projections = repmat(FDA_scores,1,size(signed_diff,2)).*repmat(DA_direction',size(signed_diff,1),1);
 
 %removing the DA projection for each face from its signed difference. This
 %gives the asymmetry vector for each face, corrected for individual DA
